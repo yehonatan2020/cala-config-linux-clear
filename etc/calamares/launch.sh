384eda9bf4e3e7cd386b07c2b1e6d0e08ce70865
yehonatan2020 <yehonatan@yandex.ru>
2023-12-16 17:44:58 +0000

    Changes necessary for Linux-Clear kernel

--- etc/calamares/launch.sh
+++ etc/calamares/launch.sh
@@ -5,7 +5,7 @@ KERNEL=`uname -r`
 
 if [[ -d "/run/archiso/copytoram" ]]; then
 	sudo sed -i -e 's|/run/archiso/bootmnt/arch/x86_64/airootfs.sfs|/run/archiso/copytoram/airootfs.sfs|g' "$DIR"/modules/unpackfs.conf
-	sudo sed -i -e "s|/run/archiso/bootmnt/arch/boot/x86_64/vmlinuz-linux-amd-znver2|/usr/lib/modules/$KERNEL/vmlinuz-amd-znver2|g" "$DIR"/modules/unpackfs.conf
+	sudo sed -i -e "s|/run/archiso/bootmnt/arch/boot/x86_64/vmlinuz-linux-clear|/usr/lib/modules/$KERNEL/vmlinuz-clear|g" "$DIR"/modules/unpackfs.conf
 fi
 
 sudo pkexec calamares -D8
